9ee16aaad19b909d6a7621c2be53339f532fb525
GliderTech <71361118+michaelglider@users.noreply.github.com>
2024-02-09 17:34:07 -0800

    Add files via upload

---
+++ backend/routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const {
+    getWorkout,
+    getWorkouts,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)
+
+//POST a new workout
+router.post('/', createWorkout)
+
+//DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+//UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+//Export the routes
+module.exports = router
